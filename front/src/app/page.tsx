011f9b78cbee66c9dce41d6605b544f9279f2082
amos-macedo <amosdemacedo007@gmail.com>
2025-09-14 15:08:28 -0300

    adding new front folder

---
+++ front/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { TaskList } from "@/components/task-list";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-800 flex items-center justify-center w-screen h-screen">
+      <TaskList />
+    </div>
+  );
+}
